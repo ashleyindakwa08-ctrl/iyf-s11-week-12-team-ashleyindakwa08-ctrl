2770ad56e30ae84b4d4162f53d5421d8a76c84aa
izokizing <izokizing@gmail.com>
2026-08-07 16:38:27 +0300

    Update MongoDB connection URI variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`MongoDB connected: ${conn.connection.host}`);
+        console.log("MongoDB connected: ${conn.connection.host}");
     } catch (error) {
-        console.error(`Database connection failed: ${error.message}`);
+        console.error("Database connection failed:", error.message);
         process.exit(1);
     }
 };
