c3d8070e657df5e974f30c03442db20f1f930516
izokizing <izokizing@gmail.com>
2026-08-07 18:19:05 +0300

    Reorder import statements in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import Auth from "./pages/Auth.jsx";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+  import Auth from "./pages/Auth.jsx";
+
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
